577a2f81fd5f07a313a04db57c918bd1976e3069
Ivan Gonzalez <43054202+igonza17@users.noreply.github.com>
2020-12-23 18:33:43 -0500

    Delete shell2.py

--- shell2.py
+++
@@ -1,9 +0,0 @@
-import ProjectMorpheus2
-while True:
-    text = input('Result > ')
-    result, error = ProjectMorpheus2.run(text)
-    if error:print(error.log())
-    elif result: print(result)
-
-
-
